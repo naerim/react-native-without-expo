10723fd51285b1b330976960db92774b008c00b8
naerim <naerim1119@gmail.com>
2021-12-16 20:41:42 +0900

    리액트 네이티브 프로젝트

---
+++ LearnReactNative/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Greeting = () => {
+  return (
+    <View>
+      <Text>Greeting</Text>
+    </View>
+  );
+};
+
+export default Greeting;
